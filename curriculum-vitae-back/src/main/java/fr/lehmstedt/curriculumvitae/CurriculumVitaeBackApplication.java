6e40f71f6627ef39d0af0c3b83ceac8e0bb1a3ff
Sébastien Lehmstedt <lehmstedt-sebastien@hotmail.fr>
2018-09-01 17:16:19 +0200

    Initialize spring boot app

---
+++ curriculum-vitae-back/src/main/java/fr/lehmstedt/curriculumvitae/CurriculumVitaeBackApplication.java
@@ -0,0 +1,12 @@
+package fr.lehmstedt.curriculumvitae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculumVitaeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurriculumVitaeBackApplication.class, args);
+	}
+}
